5dfc369bcc8573ee4a29f3f05486e97f0cd46272
Raul Victor Trombin <raulvtrombin@gmail.com>
2025-02-05 00:59:47 -0300

    test

--- builds/biqu-b1.js
+++ builds/biqu-b1.js
@@ -1,13 +1,13 @@
 module.exports = {
-    board_env: "STM32F429VG_btt",
-    min_version: "2.1.0",
+    board_env: "STM32F103RE_creality",
+    min_version: "2.1.2",
     meta: {
-        stable_name: "biqu-b1-skr-v2-{{marlin_version}}-default-{{uid}}",
-        nightly_name: "biqu-b1-skr-v2-{{current_date}}-default-{{uid}}"
+        stable_name: "ender_3_4.2.2-{{marlin_version}}-default-{{uid}}",
+        nightly_name: "ender_3_4.2.2-{{current_date}}-default-{{uid}}"
     },
     based_on: {
         repo: "https://github.com/MarlinFirmware/Configurations/",
-        path: "/config/examples/BIQU/B1/",
+        path: "/config/examples/Creality/Ender-3/CrealityV422/",
         stable_branch: "release-{{marlin_version}}",
         nightly_branch: "bugfix-2.1.x"
     }
